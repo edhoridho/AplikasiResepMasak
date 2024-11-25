74eaf60043ace9bc5f420b17e5e8c1de4a04a400
dho <edridho.dev@gmail.com>
2024-11-26 00:24:26 +0800

    fitur cetak berhasil

--- src/aplikasiresepmasakan/CetakPDF.java
+++ src/aplikasiresepmasakan/CetakPDF.java
@@ -8,37 +8,84 @@ package aplikasiresepmasakan;
  *
  * @author ACER A314
  */
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Paragraph;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CetakPDF {
-    public static void cetakSemuaResep(String dataResep) {
-        try {
-            PdfWriter writer = new PdfWriter("SemuaResep.pdf");
-            PdfDocument pdf = new PdfDocument(writer);
-            Document document = new Document(pdf);
-            document.add(new Paragraph(dataResep));
-            document.close();
-            System.out.println("PDF Semua Resep berhasil dibuat!");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+    // Path ke file font
+    private static final String FONT_PATH = "resources/fonts/Courier New Regular.ttf";
+
+    public static void cetakResep(String namaResep, String dataResep) {
+        try (PDDocument document = new PDDocument()) {
+            PDPage page = new PDPage();
+            document.addPage(page);
+
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+            contentStream.setFont(PDType0Font.load(document, new File(FONT_PATH)), 12); // Gunakan font eksternal
+            contentStream.beginText();
+            contentStream.setLeading(14.5f);
+            contentStream.newLineAtOffset(25, 700);
+
+            // Tambahkan teks
+            String[] lines = dataResep.split("\n");
+            for (String line : lines) {
+                contentStream.showText(line);
+                contentStream.newLine();
+            }
+
+            contentStream.endText();
+            contentStream.close();
+
+        // Generate nama file berbasis nama resep
+        String sanitizedNamaResep = namaResep.replaceAll("[^a-zA-Z0-9]", "_"); 
+        String fileName = "Resep_" + sanitizedNamaResep + ".pdf";
+        document.save(fileName);
+
+        System.out.println("PDF Resep berhasil dibuat dengan nama: " + fileName);
+    } catch (Exception e) {
+        System.err.println("Error saat mencetak resep:");
+        e.printStackTrace();
+    }
     }
 
-    public static void cetakResep(String dataResep) {
-        try {
-            PdfWriter writer = new PdfWriter("DetailResep.pdf");
-            PdfDocument pdf = new PdfDocument(writer);
-            Document document = new Document(pdf);
-            document.add(new Paragraph(dataResep));
-            document.close();
-            System.out.println("PDF Resep berhasil dibuat!");
-        } catch (Exception e) {
-            e.printStackTrace();
+public static void cetakSemuaResep(String dataResep) {
+    try (PDDocument document = new PDDocument()) {
+        PDPage page = new PDPage();
+        document.addPage(page);
+
+        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+        contentStream.setFont(PDType0Font.load(document, new File(FONT_PATH)), 12);
+        contentStream.beginText();
+        contentStream.setLeading(14.5f);
+        contentStream.newLineAtOffset(25, 700);
+
+        // Tambahkan teks
+        String[] lines = dataResep.split("\n");
+        for (String line : lines) {
+            contentStream.showText(line);
+            contentStream.newLine();
         }
+
+        contentStream.endText();
+        contentStream.close();
+
+        // Generate nama file unik
+        String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        String fileName = "SemuaResep_" + timestamp + ".pdf";
+        document.save(fileName);
+
+        System.out.println("PDF Semua Resep berhasil dibuat dengan nama: " + fileName);
+    } catch (Exception e) {
+        System.err.println("Error saat mencetak semua resep:");
+        e.printStackTrace();
     }
+}
 }
 
 
